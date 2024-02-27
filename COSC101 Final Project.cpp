6703129d410e8625f3816403bdf01ca1429eb482
Favour A. Uzochukwu <uzochukwu.favour310@gmail.com>
2024-02-27 15:21:18 +0100

    Upload of the userDB file and implementation of the creation of a new user in the system

--- COSC101 Final Project.cpp
+++ COSC101 Final Project.cpp
@@ -1,72 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX_USERS 100
 #define MAX_NAME 20
 #define MAX_PASSCODE 5
 
-// Structure to represent a user
-typedef struct {
-    char name[MAX_NAME];
-    int passCode[MAX_PASSCODE];
-} User;
+void addUser(char name[], int passcode[]);
 
 // Function to add user to the database
 void addUser(char name[], int passcode[]) {
-    FILE *userDB = fopen("../Desktop/userDB.txt", "a");
+    FILE *userDB = fopen("userDB.txt", "a+");
     if (userDB == NULL) {
-        printf("Error Creating File");
+        perror("Error Creating File");
         return;
     }
-    fprintf(userDB, "Name: %s - Passcode: %d", name, passcode);
+    fprintf(userDB, "Name: %s - Passcode: %d\n", name, passcode);
     fclose(userDB);
 }
 
 int main() {
     int availableAttempts = 3;
     char name[MAX_NAME];
-    int passcode[MAX_PASSCODE];
+    int passcode;
     int loginPrompt;
     
-    FILE *userDB = fopen("../Desktop/userDB.txt", "re");
+    FILE *userDB = fopen("userDB.txt", "ra+");
 
-    while (loginPrompt != 1 || loginPrompt != 2) {
-        printf("\n***Press:\n 1 to login\n 2 to set up a new user\n");
-        scanf("%d", &loginPrompt);
-    }
+    printf("\n***\nPress:\n 1 to login\n 2 to set up a new user\n***");
+    scanf("%d", &loginPrompt);
+    getchar(); // Consume newline character
 
     if (loginPrompt == 1) {
         // Code for login
         printf(" ***Welcome Back Home!***\nHope you had a lovely day\nInput your name to proceed: ");
-        fgets(name, sizeof(name), STDIN);
+        fgets(name, sizeof(name), stdin);
         name[strcspn(name, "\n")] = 0; 
         
-    } else {
-        // Code for setting up a new user
-    }
+    } else if(loginPrompt == 2) {
+    	printf("Input your name to register new user: ");
+    	fgets(name, sizeof(name), stdin);
+        name[strcspn(name, "\n")] = 0; 
+        
+        printf("Input your your 5-digit passcode: ");
+        scanf("%d", &passcode);
+        
+        addUser(name, &passcode);
+        printf("User successfully created");
+    } else{
+    	printf("%d is not an option\n", loginPrompt);
+    	main();
+	}
 
 	fclose(userDB);
     return 0;
 }
 
-
-	
-	if (name == storedName){
-		while(availableAttempts != 0) 
-		{
-			printf("Input the passcode to proceed");
-			scanf("%d", &passCode);
-			
-			sprintf(passCodeString, "%d", passCode);
-			if (strlen(passCodeString) != 5)
-			{
-				printf("Wrong Password!");
-				availableAttempts --;
-			}
-			else if ()
-			{
-				
-			}
-		}
-	}
-}
